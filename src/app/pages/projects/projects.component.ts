223cc37f1907a0eba69036cc00a6199a8a734fac
ahmed elayek <ahmedelayek7@gmail.com>
2023-10-02 04:02:42 +0300

    convert images to webp

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -8,70 +8,70 @@ import { Component } from '@angular/core';
 export class ProjectsComponent {
   projects = [
     {
-      src: "assets/projects-images/mini-dashboard.png",
+      src: "assets/projects-images/mini-dashboard.webp",
       name: "Mini Dashboard",
       codeUrl: "https://github.com/ahmedelayk/mini-dashboard",
       demoUrl: "https://dashboard-ahmedelayk.vercel.app/home",
       techUsed: ["Angular", "bootstrap", "apex-charts"]
     },
     {
-      src: "assets/projects-images/portfolio.png",
+      src: "assets/projects-images/portfolio.webp",
       name: "My Portfolio",
-      codeUrl: "https://github.com/ahmedelayk/portfolio-angular",
-      demoUrl: "https://student-dashboard-sgnm-ahmedelayk.vercel.app/students",
+      codeUrl: "https://github.com/ahmedelayk/portfolio",
+      demoUrl: "https://portfolio-ahmedelayk.vercel.app/",
       techUsed: ["Angular", "bootstrap"]
     },
     {
-      src: "assets/projects-images/student-dashboardd.png",
+      src: "assets/projects-images/student-dashboardd.webp",
       name: "Students Dashboard",
       codeUrl: "https://github.com/ahmedelayk/Student-Dashboard",
       demoUrl: "https://student-dashboard-sgnm-ahmedelayk.vercel.app/students",
       techUsed: ["Angular", "bootstrap", "json-server"]
     },
     {
-      src: "assets/projects-images/healthify.png",
+      src: "assets/projects-images/healthify.webp",
       name: "Healthify",
       codeUrl: "https://github.com/ahmedelayk/Healthify-Project",
       demoUrl: "https://healthify-ten.vercel.app/",
       techUsed: ["ReactJs", "Redux", "Firebase", "react-bootstrap"]
     },
     {
-      src: "assets/projects-images/movies.png",
+      src: "assets/projects-images/movies.webp",
       name: "Trending Movies",
       codeUrl: "https://github.com/ahmedelayk/TrendingMovies",
       demoUrl: "https://trending-movies-six.vercel.app/",
       techUsed: ["ReactJs", "Bootstrap"]
     },
     {
-      src: "assets/projects-images/todo.png",
+      src: "assets/projects-images/todo.webp",
       name: "To Do app",
       codeUrl: "https://github.com/ahmedelayk/ToDoListApp",
-      demoUrl: "https://dashboard-ahmedelayk.vercel.app/home",
+      demoUrl: "https://to-do-list-app-ahmedelayk.vercel.app/",
       techUsed: ["Html", "Css", "JavaScript"]
     },
     {
-      src: "assets/projects-images/products.png",
+      src: "assets/projects-images/products.webp",
       name: "Products cart",
       codeUrl: "https://github.com/ahmedelayk/StoreApp",
       demoUrl: "https://store-app-iota.vercel.app/",
       techUsed: ["Html", "Css", "JavaScript"]
     },
     {
-      src: "assets/projects-images/agency-template2.png",
+      src: "assets/projects-images/agency-template2.webp",
       name: "Agency Template 1",
       codeUrl: "https://github.com/ahmedelayk/OnePageTemplate",
       demoUrl: "https://template-gilt.vercel.app/",
       techUsed: ["Html", "Css", "JavaScript"]
     },
     {
-      src: "assets/projects-images/agency-template.png",
+      src: "assets/projects-images/agency-template.webp",
       name: "Agency Template 2",
       codeUrl: "https://github.com/ahmedelayk/Agency-Template",
       demoUrl: "https://agency-template-blond.vercel.app/",
       techUsed: ["Html", "Css"]
     },
     {
-      src: "assets/projects-images/agency-template1.png",
+      src: "assets/projects-images/agency-template1.webp",
       name: "Agency Template 3",
       codeUrl: "https://github.com/ahmedelayk/Kasper-Template",
       demoUrl: "https://kasper-template-jet.vercel.app/",
--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -8,81 +8,123 @@ import { Component } from '@angular/core';
 export class SkillsComponent {
   skills = [
     {
-      src: "assets/skills-logos/angularr.png",
+      src: "assets/skills-logos/angular.webp",
       name: "angular",
       bgColor: "#dd1b16",
       textColor: "#FFF"
     },
     {
-      src: "assets/skills-logos/ts.png",
+      src: "assets/skills-logos/ts.webp",
       name: "typescript",
       bgColor: "#3178c6",
       textColor: "#FFF"
     },
     {
-      src: "assets/skills-logos/js.png",
+      src: "assets/skills-logos/js.webp",
       name: "javascript",
       bgColor: "#f0db4f",
       textColor: "#323330"
     },
     {
-      src: "assets/skills-logos/HTML5.png",
+      src: "assets/skills-logos/html.webp",
       name: "HTML",
       bgColor: "#e34c26",
       textColor: "#fff"
     },
     {
-      src: "assets/skills-logos/CSS3.png",
+      src: "assets/skills-logos/css.webp",
       name: "CSS",
       bgColor: "#0074d9",
       textColor: "#fff"
     },
     {
-      src: "assets/skills-logos/Bootstrap.png",
+      src: "assets/skills-logos/bootstrap.webp",
       name: "Bootstrap",
       bgColor: "#563d7c",
       textColor: "#fff"
     },
     {
-      src: "assets/skills-logos/Sass.png",
+      src: "assets/skills-logos/sass.webp",
       name: "Sass",
       bgColor: "#cc6699",
       textColor: "#fff"
     },
     {
-      src: "assets/skills-logos/tailwindcss.png",
-      name: "tailwind",
-      bgColor: "#82bcf1",
-      textColor: "#fff"
-    },
-    {
-      src: "assets/skills-logos/Git.png",
+      src: "assets/skills-logos/git.webp",
       name: "Git",
       bgColor: "#f34f29",
       textColor: "#fff"
     },
     {
-      src: "assets/skills-logos/Github.png",
+      src: "assets/skills-logos/github.webp",
       name: "Github",
       bgColor: "#7f7f7f",
       textColor: "#fff"
     },
     {
-      src: "assets/skills-logos/React.png",
+      src: "assets/skills-logos/vscode.webp",
+      name: "VsCode",
+      bgColor: "#227EE5",
+      textColor: "#fff"
+    },
+    {
+      src: "assets/skills-logos/tailwind.webp",
+      name: "tailwindcss",
+      bgColor: "#82bcf1",
+      textColor: "#fff"
+    },
+    {
+      src: "assets/skills-logos/react.webp",
       name: "ReactJs",
       bgColor: "#88dded",
       textColor: "#212121"
     },
     {
-      src: "assets/skills-logos/redux.png",
+      src: "assets/skills-logos/redux.webp",
       name: "redux",
       bgColor: "#764abc",
       textColor: "#fff"
     },
     {
-      src: "assets/skills-logos/Figma.png",
+      src: "assets/skills-logos/figma.webp",
       name: "Figma",
       bgColor: "#1abcfe",
+      textColor: "#fff"
+    },
+    {
+      src: "assets/skills-logos/ps.webp",
+      name: "Photoshop",
+      bgColor: "#002935",
+      textColor: "#13D5FC"
+    },
+    {
+      src: "assets/skills-logos/ai.webp",
+      name: "Illustrator",
+      bgColor: "#331407",
+      textColor: "#FF8E27"
+    },
+    {
+      src: "assets/skills-logos/csharp.webp",
+      name: "CSharp",
+      bgColor: "#9B4F97",
+      textColor: "#fff"
+    },
+    {
+      src: "assets/skills-logos/cplusplus.webp",
+      name: "C++",
+      bgColor: "#007FCC",
+      textColor: "#fff"
+    },
+    {
+      src: "assets/skills-logos/firebase.webp",
+      name: "Firebase",
+      bgColor: "#FFCA28",
+      textColor: "#fff"
+    },
+    {
+      src: "assets/skills-logos/sql.webp",
+      name: "SQL",
+      bgColor: "#2687CF",
       textColor: "#fff"
     },
   ]
